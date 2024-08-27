4753cbf6fc2de9fe262b341f106b1c771cf91257
prathameshpatankar303 <86076082+prathameshpatankar303@users.noreply.github.com>
2024-08-27 12:17:54 +0000

    scope

--- .devcontainer/01-basics/04-scope.js
+++ .devcontainer/01-basics/04-scope.js
@@ -17,3 +17,51 @@ if (true) {
 //scope in if statement is block scope
 // scope ouside is called global scope
 // scopes are diff when run in browser and code space
+
+
+//-----------------------------------------------------------------------------------------------------------------------
+//closure - child can access parent elements but not vise versa
+
+
+function one(){
+
+    const username = "pratha"
+
+    function two(){
+
+        const website = "youtube"
+        console.log(username);
+        
+    }
+    two()
+}
+
+one()
+
+
+//-----------------------------------------------------------------------------------------------------------------------
+//two types of how to declare a function:
+//first type: can give the result even if the function is called before even declaring it
+
+
+function addone(num){
+
+    return num + 1
+
+}
+
+console.log(addone(3))
+
+
+//second type:  cannot give the result if the function is called before even declaring it
+
+
+const addTwo = function(num){
+
+    return num + 2
+}
+
+console.log(addTwo(4));
+
+
+
